59897627881ef4060997d4327289e49c3fd354dd
Harish Thakkallapally <hthakkallapally@paypal.com>
2015-07-08 14:58:50 -0700

    Update LICENSE. Remove unused imports

--- src/main/java/org/jenkins/plugins/statistics/util/JSONUtil.java
+++ src/main/java/org/jenkins/plugins/statistics/util/JSONUtil.java
@@ -2,10 +2,6 @@ package org.jenkins.plugins.statistics.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
-import java.util.Map;
 
 /**
  * Created by hthakkallapally on 3/17/2015.
