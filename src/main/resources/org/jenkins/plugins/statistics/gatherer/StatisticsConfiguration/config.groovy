d2c612d9fcbd5f33515314c2eac4540d442e7754
Maxime Charron <maxime.charron@frimastudio.com>
2016-07-25 10:24:29 -0400

    Fix bad merge

--- src/main/resources/org/jenkins/plugins/statistics/gatherer/StatisticsConfiguration/config.groovy
+++ src/main/resources/org/jenkins/plugins/statistics/gatherer/StatisticsConfiguration/config.groovy
@@ -4,19 +4,19 @@ def f=namespace(lib.FormTagLib)
 
 f.section(title:_("Statistics Gatherer")) {
     f.entry(title:_("Queue URL"), field:"queueUrl") {
-        f.textbox(default: "https://prodmetrics.frimastudio.com/queue")
+        f.textbox(default: "http://ci.mycompany.com/api/queue")
     }
     f.entry(title:_("Build URL"), field:"buildUrl") {
-        f.textbox(default: "https://prodmetrics.frimastudio.com/build")
+        f.textbox(default: "http://ci.mycompany.com/api/build")
     }
     f.entry(title:_("Project URL"), field:"projectUrl") {
         f.textbox(default: "http://ci.mycompany.com/api/projects")
     }
     f.entry(title:_("BuildSteps URL"), field:"buildStepUrl") {
-        f.textbox(default: "https://prodmetrics.frimastudio.com/jenkins/build/step")
+        f.textbox(default: "http://ci.mycompany.com/api//step")
     }
     f.entry(title:_("ScmCheckoutInfo URL"), field:"scmCheckoutUrl") {
-        f.textbox(default: "https://prodmetrics.frimastudio.com/jenkins/build/scm")
+        f.textbox(default: "http://ci.mycompany.com/api//scm")
     }
 
     f.entry(title:_("Send Queue Info"), field:"queueInfo") {
@@ -26,7 +26,7 @@ f.section(title:_("Statistics Gatherer")) {
         f.checkbox(default: true)
     }
     f.entry(title:_("Send Job Info"), field:"projectInfo") {
-        f.checkbox(default: false)
+        f.checkbox(default: true)
     }
     f.entry(title:_("Send BuildSteps Info"), field:"buildStepInfo") {
         f.checkbox(default: true)
