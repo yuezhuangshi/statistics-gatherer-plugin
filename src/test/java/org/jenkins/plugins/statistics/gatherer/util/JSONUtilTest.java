6500880abb71ea016ac04d8d59a71100f8ed5b9c
Maxime Charron <charron.maxime97@gmail.com>
2016-07-27 08:39:32 -0400

    Added JSonUtil tests

--- src/test/java/org/jenkins/plugins/statistics/gatherer/util/JSONUtilTest.java
+++ src/test/java/org/jenkins/plugins/statistics/gatherer/util/JSONUtilTest.java
@@ -2,7 +2,10 @@ package org.jenkins.plugins.statistics.gatherer.util;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.jenkins.plugins.statistics.gatherer.model.build.SCMInfo;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -12,6 +15,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -37,15 +42,46 @@ public class JSONUtilTest {
         assertEquals(expectedJson, jsonString);
     }
 
-//    @Test(expected=JsonParseException.class)
-//    public void givenInvalidObject_whenToJson_thenThrows() throws IOException{
-//        testObject.setUrl("http://test.com");
-//        testObject.setBranch("blop");
-//        testObject.setCommit("blopie");
-//        ObjectMapper mapper = Mockito.mock(ObjectMapper.class);
-//        Mockito.doThrow(new JsonParseException(null, "error"))
-//                .when(mapper).writeValueAsString(Matchers.anyObject());
-//        JSONUtil.convertToJson(testObject);
-//
-//    }
+    @Test
+    public void givenInvalidObject_whenToJson_thenReturnEmptyJson(){
+        JSONObject object = new JSONObject();
+        String json = JSONUtil.convertToJson(object);
+        assertEquals("", json);
+    }
+
+    @Test
+    public void givenJsonArray_whenConvertToList_thenReturnList(){
+        JSONArray array = new JSONArray();
+        array.put("test");
+        List<String> result = JSONUtil.convertJsonArrayToList(array);
+        assertEquals(1, result.size());
+        assertEquals("test", result.get(0));
+    }
+
+    @Test
+    public void givenNull_whenConvertToList_thenReturnEmptyList(){
+        List<String> result = JSONUtil.convertJsonArrayToList(null);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void givenJsonObjectWithCategories_whenConvertBuildFailureToMap_thenReturnValidMap(){
+        JSONArray array = new JSONArray();
+        array.put("test");
+        JSONObject object = new JSONObject();
+        object.put("categories", array);
+        Map<String, Object> result = JSONUtil.convertBuildFailureToMap(object);
+        assertEquals(1, result.size());
+        assertEquals(1, ((List)result.get("categories")).size());
+        assertEquals("test", ((List)result.get("categories")).get(0));
+    }
+
+    @Test
+    public void givenJsonObjectWithoutCategories_whenConvertBuildFailureToMap_thenReturnValidMap(){
+        JSONObject object = new JSONObject();
+        object.put("stuff", "testString");
+        Map<String, Object> result = JSONUtil.convertBuildFailureToMap(object);
+        assertEquals(1, result.size());
+        assertEquals("testString", result.get("stuff"));
+    }
 }
